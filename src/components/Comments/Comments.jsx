a5e8f1ce6c10fab348f41c2027c01b17aca05429
mortezatava <morteza.tavakoli11@gmail.com>
2024-03-23 03:28:39 -0700

    removing commented code

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,41 +1,3 @@
-// import './Comments.scss'
-// import Comment from "../Comment/Comment"
-// import avatar from '../../assets/Images/Mohan-muruge.jpg'
-// import add_comment from '../../assets/Icons/add_comment.svg'
-// function Comments(props) {
-
-//     return (
-//         <>
-//             <h4 className='comments-form__number'>3 Comments</h4>
-//             <h4 className='comments-form__title'>JOIN THE CONVERSATION</h4>
-//             <div className='commentsForm-container'>
-//                 <img className="avatar" src={avatar} alt="avatar" />
-//                 <textarea name="comment" className='comments-form__input' placeholder="Add a new comment">
-//                 </textarea>
-
-//                 <div className='forms_submit' >
-//                     <button type="submit" className='submit-button' >
-//                         <img src={add_comment} alt="add-comment" />
-//                         <span className="button-text">Comment</span>
-//                     </button>
-//                 </div>
-//                 <div className='comments-list'>
-
-//                 </div>
-
-//             </div>
-
-//             {props.selectedVideoComments?.comments?.map(commentDetail =>
-//                 <Comment comment={commentDetail} />
-
-//             )
-//             }
-//         </>
-
-//     )
-// }
-
-// export default Comments
 
 import './Comments.scss'
 import Comment from "../Comment/Comment"
@@ -67,9 +29,6 @@ function Comments(props) {
 
             )
             }
-
-
-
         </>
 
     )
